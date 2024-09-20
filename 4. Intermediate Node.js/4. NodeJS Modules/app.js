929288b2bd5a7d5820bebe147c61aa13c2ade4e5
Faizululum <faizululum25@gmail.com>
2024-09-20 16:53:13 +0700

    Express basic

--- 4. Intermediate Node.js/4. NodeJS Modules/app.js
+++ 4. Intermediate Node.js/4. NodeJS Modules/app.js
@@ -3,15 +3,24 @@ const http = require('http');
 const app = express();
 
 // Membuat server HTTP sederhana
-const server = http.createServer((req, res) => {
+// const server = http.createServer((req, res) => {
     // Menangani permintaan dari client
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!');
-});
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello, world!');
+// });
 
+// Express js
+// Rute Beranda
 // Mendengarkan pada port tertentu
 const port = 3000;
-server.listen(port, () => {
+
+app.get('/', (req, res) => {
+    res.send("Selamat datang di halaman beranda");
+});
+
+app.listen(port, () => {
     console.log(`Server berjalan di http://localhost:${port}`);
 });
+
+//
