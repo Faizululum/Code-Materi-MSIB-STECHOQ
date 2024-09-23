94e19b5d746373c042b0df94e9258cbd23f0fd3d
Faizululum <faizululum25@gmail.com>
2024-09-24 06:09:54 +0700

    Routing

--- Belajar Express (PZN)/src/index.js
+++ Belajar Express (PZN)/src/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+// Routing
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About');
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
