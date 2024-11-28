a6236beb72c942bd39867d64c9d22410ed2ab484
Faizululum <faizululum25@gmail.com>
2024-11-29 06:20:41 +0700

    Backend Project Deployment using Vercel

--- frontend/src/plugins/piniaPersist.js
+++
@@ -1,9 +0,0 @@
-export function piniaPersistPlugin(context) {
-    const storedState = localStorage.getItem(`pinia-${context.store.$id}`);
-    if (storedState) {
-        context.store.$patch(JSON.parse(storedState));
-    }
-    context.store.$subscribe((mutation, state) => {
-        localStorage.setItem(`pinia-${context.store.$id}`, JSON.stringify(state));
-    });
-}
